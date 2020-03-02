a95a502e83aa5c44a3cb2072784b1886ae7a4918
root <camahui21@gmail.com>
2020-03-01 21:36:57 -0600

    updating app

--- server.R
+++ server.R
@@ -3,7 +3,7 @@ options(shiny.maxRequestSize = 9*1024^2)
 function(input,output,session){
 
     output$serie<-renderDygraph({
-        dygraph(TS, main="World Coronavirus Evolution from Daily WHO Data") %>% dyAxis("y", label="Indivuduals", valueRange=c(0,max(Datos$n)+1000)) %>% dyRangeSelector() %>% dyLegend(labelsDiv = "legendDivID", labelsSeparateLines = T) %>% dyHighlight(highlightSeriesOpts = list(strokeWidth = 3)) %>% dyOptions(axisLineWidth = 1.5, fillGraph = TRUE, drawGrid = T)#, colors = rev(RColorBrewer::brewer.pal(5, "Set1")[3:5]))
+        dygraph(TS, main="World Coronavirus Evolution from Daily WHO Data", height=600) %>% dyAxis("y", label="Indivuduals", valueRange=c(0,max(Datos$n)+1000)) %>% dyRangeSelector() %>% dyLegend(labelsDiv = "legendDivID", labelsSeparateLines = T) %>% dyHighlight(highlightSeriesOpts = list(strokeWidth = 3)) %>% dyOptions(axisLineWidth = 1.5, fillGraph = TRUE, drawGrid = T)#, colors = rev(RColorBrewer::brewer.pal(5, "Set1")[3:5]))
     })
     
     output$reporte<-renderUI({
--- ui.R
+++ ui.R
@@ -22,14 +22,15 @@ dashboardPage(skin="red",
             tabItem(tabName="mon",
                 tags$style(HTML(".box.box-solid.box-primary>.box-header {  color:#fff;  background:#eb4034} .box.box-solid.box-primary{border-bottom-color:#eb4034;border-left-color:#eb4034;border-right-color:#eb4034;border-top-color:#eb4034;}")),
                 fluidRow(
-                    box(width=12, height=600, title="Tracking", status="primary", solidHeader=T,
+                    box(width=9, height=600, title="Tracking", status="primary", solidHeader=T,
                         p(),
                         p(),
                         p(),
-                        column(9,
-                            dygraphOutput("serie")
+#                        column(9,
+                            dygraphOutput("serie", height=530)
                         ),
-                        column(3,
+#                        column(3,
+                    box(width=3, height=600, title="Tracking", status="primary", solidHeader=T,                        
                             h4("Instructions:"),
                             HTML("<p>Hover over the graph to show in the box below the absolute frequencies of infected and death individuals, and the proportion of deaths in relation to the daily total of infected people.</p>"),
                             br(),
@@ -37,8 +38,8 @@ dashboardPage(skin="red",
                             br(),
                             HTML("<p>The information shown in this web app comes from the <a href=https://www.who.int/emergencies/diseases/novel-coronavirus-2019/situation-reports/>World Health Organization daily reports</a>. It was built by <a href=https://datametrix.co/servicios/>Datametrix</a>, using shinydashboard and dygraphs, and the daily web scrapping of the reports is performed using RCurl and rvest.</p>"),
                             br(),
-                            HTML("<p><b>Since February 17th the confirmed infected definition used by the chinese government to report its data to WHO has changed. That's why a dramatic increase in the total number of infected cases can be observed in comparisson with the quantity reported on February 16th and the previous days.</b></p>")
-                        )
+                            HTML("<p><b>Since February 17th the confirmed infected definition used by the chinese government has changed. That's why a conspicuous increase in the total number of infected cases can be observed in comparisson with the quantities reported before.</b></p>")
+#                        )
                     ),
                     box(width=12, height=831, title="World Health Organization Report", status="primary", solidHeader=T,
                             br(),
